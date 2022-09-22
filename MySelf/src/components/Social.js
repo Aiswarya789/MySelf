4c6dcde7672174c6dbcba4930c9d0c64133b50dd
Aiswarya789 <114132947+Aiswarya789@users.noreply.github.com>
2022-09-22 11:57:19 +0530

    Add files via upload

---
+++ MySelf/src/components/Social.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://codepen.io/abhinav3432" target="_blank" rel="noopener noreferrer"><i class="fab fa-codepen"></i></a>
+            <a href="https://github.com/Aiswarya789/" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/_aiwa_aira/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/AS IF/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://Linkedin.com/in/ASIF NAVAS/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@abhinav234678" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
+    
---
+++ MySelf/src/contents/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Social from '../components/Social';
+class Contact extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">Contact Me</h1>
+            <h3>Email  :   aiswaryadeepa92@gmail.com</h3>
+            <h3>Instagram   :   aiwa_aira</h3>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Contact
+    
